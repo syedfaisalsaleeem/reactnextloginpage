533c074a9ecc0b80dfbdf482130353e2c40fbb47
SyedFaisalSaleeem <syedfaisalsaleem.100@gmail.com>
2020-06-02 22:15:39 +0500

    track

--- pages/theme.js
+++ pages/theme.js
@@ -2,7 +2,7 @@ import { createMuiTheme } from '@material-ui/core/styles';
 import purple from '@material-ui/core/colors/purple';
 import green from '@material-ui/core/colors/green';
 
-const theme = createMuiTheme({
+export default theme = createMuiTheme({
   palette: {
     primary: purple,
     secondary: green,
