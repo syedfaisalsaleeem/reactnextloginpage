7cc634a906da55db97fa4130f5cb6d7b55e2ce22
SyedFaisalSaleeem <syedfaisalsaleem.100@gmail.com>
2020-06-02 22:12:58 +0500

    commiting

--- pages/theme.js
+++ pages/theme.js
@@ -1,5 +1,13 @@
-export default function(){
-    <div>
-        <h1>fa</h1>
-    </div>
-}
+import { createMuiTheme } from '@material-ui/core/styles';
+import purple from '@material-ui/core/colors/purple';
+import green from '@material-ui/core/colors/green';
+
+const theme = createMuiTheme({
+  palette: {
+    primary: purple,
+    secondary: green,
+  },
+  status: {
+    danger: 'orange',
+  },
+});
